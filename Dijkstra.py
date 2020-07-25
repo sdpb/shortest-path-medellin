5fe4ec3da2be8f8f4387b4e82753c67e3a944884
sdpb <sdpalaciob@eafit.edu.co>
2020-07-24 21:24:07 -0500

    Improve methods

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,16 +1,20 @@
-from matplotlib.pyplot import subplots, show
-from networkx import draw, draw_shell, dijkstra_path, dijkstra_path_length
+from matplotlib.pyplot import subplots, show, savefig
+from networkx import draw, dijkstra_path, dijkstra_path_length
 from geopandas import GeoDataFrame, read_file
 from shapely.geometry import Point, MultiPoint
 from shapely.ops import unary_union
 from itertools import combinations, permutations
-from Way import Way, Node
-from networkx import Graph as nx_graph
+from networkx import DiGraph
+from colorama import init, Fore
+
+# Locals
+from Way import Way
+from Node import Node
 
 data_frame = []
 geoms = []
 node_list = []
-raw_data = GeoDataFrame
+raw_data = GeoDataFrame()
 fp = r"Data/Corredores_para_Transporte_de_Pasajeros.shp"
 
 
@@ -83,56 +87,94 @@ def multi_intersection_process(intersects):
         return [first_point, second_point]
 
 
-def make_nodes(intersections, data_f, time, nodelist):
-    G = nx_graph()
+def make_nodes(intersections, data_f, rain, nodelist):
     for intersection in intersections:
         node_u = Node(intersection)
         for way in data_f:
             if intersection in way.INTERSECTIONS:
                 node_u.associated_ways.append(way)
         nodelist.append(node_u)
-    weight(nodelist, time, G)
-    return G
+
+    return make_graph(nodelist, rain)
 
 
-def weight(nodes, time, G):
+def make_graph(nodes, rain):
+    G = DiGraph()
     for i, j in combinations(nodes, 2):
         for _ in i.associated_ways:
             if _ in j.associated_ways:
-                G.add_edge(i, j, weight=weather(time, i, j))
+                weather_time = weather(rain, i, j)
+                G.add_edge(i, j, weight=weather_time)
+    return G
 
 
-def weather(time, i, j):
-    if time == 'soleado':
-        return i.point.distance(j.point)
-    elif time == 'lluvioso':
-        return (i.point.distance(j.point) * 3) // 2
+def weather(rain, i, j):
+    i = i.point
+    j = j.point
+    distance = i.distance(j)
+
+    if rain:
+        return distance * 35
+    else:
+        return distance * 10
 
 
-def graphics(graph, intersects):
-    fig, ax = subplots()
+def graphic_map(intersects, rt_out, sv_out):
+    global raw_data
+    fig, ax = subplots(figsize=(19, 11))
     raw_data.plot(ax=ax, color='blue')
     pts = GeoDataFrame([[intersects]], columns=['geometry'])  # GeoDataFrame
     pts.plot(ax=ax, marker='o', color='red', markersize=15)
+    if sv_out:
+        print('\nSaving map graphic...\n')
+        savefig('map.svg', format='svg', dpi=12000)
+    if rt_out:
+        show(block=False)
 
-    fig2, ax2 = subplots()
-    draw(graph, with_labels=True, ax=ax2)
-    show()
+
+def graphic_graph(graph, rt_out, sv_out):
+    fig, ax = subplots(figsize=(19, 11))
+
+    # Define node positions data structure (dict) for plotting
+
+    # node_positions = {node[0]: (node[1].point.x, -node[1].point.y) for node in graph.nodes(data=True)}
+
+    # draw(graph, pos=node_positions, with_labels=True, ax=ax, node_size=15)
+    draw(graph, with_labels=True, ax=ax, node_size=15)
+    if sv_out:
+        print('\nSaving Graph graphic...\n')
+        savefig('graph.svg', format='svg', dpi=12000)
+    if rt_out:
+        show(block=False)
 
 
-def exe(node_1=11, node_2=12):
+def write_nodelist(filename):
+    init()
+    print('\nWriting Node List in {}...\n'.format(filename))
+    print('{}INDEX\n{}Coordinates\n{}Ways intersection'
+          .format(Fore.RED, Fore.BLUE, Fore.GREEN), end='')
+    print(Fore.RESET)
+
+    with open(filename, 'w') as file:
+        file.writelines(
+            '{}{}) {}{} {}{}\n'.format(Fore.RED, index, Fore.BLUE, _.id, Fore.GREEN, str(_))
+            for index, _ in zip(range(302), node_list))
+
+
+def exe(node_1, node_2, rain, rt_out, sv_out):
     read_with_GeoPandas(fp)
     intersects = get_intersections(geoms)
-    graph = make_nodes(intersects, data_frame, 'lluvioso', node_list)
+    graph = make_nodes(intersects, data_frame, rain, node_list)
 
-    print('\n************* Node List *******************\n')
-
-    for _ in node_list:
-        print(str(_))
-
-    print('\n************* Shortest Path *******************\n')
+    write_nodelist('nodelist.txt')
+    print('\nShortest Path...\n')
     for _ in dijkstra_path(graph, node_list[node_1], node_list[node_2]):
         print(_)
 
-    # print(dijkstra_path_length(graph, node_list[11], node_list[12]))
-    graphics(graph, intersects)
+    print('\nLength from {} to {} is: {}'.format(
+        node_1, node_2, dijkstra_path_length(graph, node_list[node_1], node_list[node_2])))
+
+    graphic_map(intersects, rt_out, sv_out)
+    graphic_graph(graph, rt_out, sv_out)
+    print('Successfully saved. Have a nice day ;)')
+    show()
